ce0ca6ea28163f1391546f746260de22ba17b7fe
Igor Siergiej <igorsiergiej@gmail.com>
2023-10-09 20:26:23 +0100

    Added cors to whitelist api address

--- database/index.js
+++ database/index.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
 const port = 3000
+const cors = require("cors");
+
+app.use(cors({
+    origin: 'https://shoppingo-api.onrender.com'
+}));
 
 app.use(bodyParser.json())
 app.use(
