63a61258ba68d24efdcfc7625cb3f60914c6152e
Igor Siergiej <igorsiergiej@gmail.com>
2023-10-20 14:38:26 +0100

    Hid away database connection details

--- database/queries.js
+++ database/queries.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const Pool = require('pg').Pool
+
 const pool = new Pool({
-    user: 'igor-siergiej',
-    host: 'ep-long-resonance-24372392.eu-central-1.aws.neon.tech',
-    database: 'neondb',
-    password: '7Ljcxg0uTpBZ',
-    port: 5432,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
     ssl: true
 })
 
