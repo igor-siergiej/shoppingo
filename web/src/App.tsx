394b21c8241bcbc743cbccc8336bb47f56f7cd4f
Igor Siergiej <igorsiergiej@gmail.com>
2023-10-20 14:25:07 +0100

    Updated calling getting all items

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,74 +5,69 @@ import './App.css';
 import { useQuery } from 'react-query';
 
 interface Item {
-  name: string;
-  isSelected: boolean;
+    name: string;
+    selected: boolean;
 }
 
 function App(): ReactElement {
-  const [show, setShow] = useState(false);
-  const { data, isLoading, isError } = useQuery('get_all_items', fetchItems);
-  return (
-    <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo.toString()} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-          <button
-            onClick={() => {
-              setShow(!show);
-            }}>
-            Get all Items
-          </button>
-        </header>
-        {show && itemComponent(data, isLoading, isError)}
-      </div>
-    </>
-  );
+    const [show, setShow] = useState(false);
+    const { data, isLoading, isError } = useQuery('get_all_items', fetchItems);
+    return (
+        <>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo.toString()} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        Learn React
+                    </a>
+                    <button
+                        onClick={() => {
+                            setShow(!show);
+                        }}>
+                        Get all Items
+                    </button>
+                </header>
+                {show && itemComponent(data, isLoading, isError)}
+            </div>
+        </>
+    );
 }
 
 function itemComponent(data: any, isLoading: boolean, isError: boolean) {
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
 
-  if (isError) {
-    return <div>Error fetching data.</div>;
-  }
+    if (isError) {
+        return <div>Error fetching data.</div>;
+    }
 
-  console.log(data);
+    console.log(data);
 
-  const items: Item[] = data.map(({ item_name, is_selected }) => ({
-    name: item_name,
-    isSelected: is_selected
-  }));
+    const renderedOutput = data.map((item: Item, index: number) => (
+        <div key={index}>
+            {index}: {item.name}, {item.selected ? 'true' : 'false'}
+        </div>
+    ));
 
-  console.log(items);
-
-  const renderedOutput = items.map((item: Item, index: number) => (
-    <div key={index}>
-      {index}: {item.name}
-    </div>
-  ));
-
-  return <div>{renderedOutput}</div>;
+    return <div>{renderedOutput}</div>;
 }
 
+const URL = 'https://shoppingo-api.onrender.com';
+
 async function fetchItems() {
-  const response = await fetch('https://shoppingo-api.onrender.com/users');
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return await response.json();
+    const response = await fetch(`${URL}/items`);
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return await response.json();
 }
 
 export default App;
