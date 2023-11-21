ee452acfcaba110913e8fac9e1a79a0c0c2afeb1
Igor Siergiej <igorsiergiej@gmail.com>
2023-11-21 21:17:47 +0000

    updated gitignore and outDir

---
+++ database/index.ts
@@ -0,0 +1,39 @@
+import express, { Application } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { getAllItems } from './queries';
+
+const app: Application = express();
+const port: number = 3001;
+
+const allowedOrigins: string[] = [
+  'https://im-shoppingo.netlify.app',
+  'http://localhost:3000',
+];
+
+const corsOptions: cors.CorsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) {
+      return 
+    }
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/items', getAllItems);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
