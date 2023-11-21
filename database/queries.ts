e134031a683c9fdd03b8f0967b964edb1803f12b
Igor Siergiej <igorsiergiej@gmail.com>
2023-11-21 20:59:25 +0000

    Reformated to typescript

---
+++ database/queries.ts
@@ -0,0 +1,31 @@
+
+import { Pool, QueryResult } from 'pg';
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: parseInt(process.env.PG_PORT || '5432', 10),
+    ssl: true,
+});
+
+const getAllItems = (request: Request, response: Response): void => {
+    pool.query(
+        `
+    SELECT shopping_list.get_all_items();
+    `,
+        (error: Error, results: QueryResult) => {
+            if (error) {
+                throw error;
+            }
+            response.status(200).json(results.rows[0].get_all_items);
+        }
+    );
+};
+
+export { getAllItems };
