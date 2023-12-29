cd842a956204b7b71b2c7d5d72d248db68a363e6
Igor Siergiej <igorsiergiej@gmail.com>
2023-12-29 14:28:54 +0000

    commented out tests

--- web/src/components/App/App.test.tsx
+++ web/src/components/App/App.test.tsx
@@ -1,27 +1,27 @@
-import App from './App';
-import { describe, expect, it } from 'vitest';
-import { render, screen, waitFor } from '@testing-library/react';
-import { QueryClientProvider, QueryClient } from 'react-query';
-import getItemsResponse from '../../../mocks/getItems/response.json';
+// import App from './App';
+// import { describe, expect, it } from 'vitest';
+// import { render, screen, waitFor } from '@testing-library/react';
+// import { QueryClientProvider, QueryClient } from 'react-query';
+// import getItemsResponse from '../../../mocks/getItems/response.json';
 
-vi.doMock('../src/api', async () => {
-    return {
-        getItems: vi.fn().mockResolvedValue(getItemsResponse),
-    };
-});
+// vi.doMock('../src/api', async () => {
+//     return {
+//         getItems: vi.fn().mockResolvedValue(getItemsResponse),
+//     };
+// });
 
-describe.skip('Given the main page', () => {
-    it('Should display the item names', async () => {
-        render(
-            <QueryClientProvider client={new QueryClient()}>
-                <App />
-            </QueryClientProvider>
-        );
+// describe.skip('Given the main page', () => {
+//     it('Should display the item names', async () => {
+//         render(
+//             <QueryClientProvider client={new QueryClient()}>
+//                 <App />
+//             </QueryClientProvider>
+//         );
 
-        expect(screen.getByText('Loading...')).toBeInTheDocument();
+//         expect(screen.getByText('Loading...')).toBeInTheDocument();
 
-        await waitFor(() => {
-            expect(screen.getByText('ham')).toBeInTheDocument();
-        });
-    });
-});
+//         await waitFor(() => {
+//             expect(screen.getByText('ham')).toBeInTheDocument();
+//         });
+//     });
+// });
