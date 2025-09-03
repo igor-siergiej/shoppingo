51cc4d47c44fd657691d9baafe006b8506a7fa80
Igor Siergiej <igorsiergiej@gmail.com>
2025-09-03 20:10:09 +0100

    make items loading skeleton look better

--- packages/web/src/components/LoadingSkeleton/index.tsx
+++ packages/web/src/components/LoadingSkeleton/index.tsx
@@ -32,6 +32,7 @@ export const ListsSkeleton = () => {
                                 <div className="flex-1">
                                     <Skeleton className="h-12 w-full" />
                                 </div>
+
                             </CardContent>
                         </Card>
                     ))}
@@ -44,19 +45,16 @@ export const ListsSkeleton = () => {
 export const ItemsSkeleton = () => {
     return (
         <>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(value => (
+            {[1, 2, 3, 4, 5, 6].map(value => (
                 <Card
                     key={value}
-                    className="mb-2 transition-all duration-200 bg-background py-0"
+                    className="mb-2 bg-background py-0"
                 >
-                    <CardContent className="flex items-center justify-between p-1 gap-4">
-                        <div className="flex items-center gap-8 flex-1">
-                            <Skeleton className="h-5 w-5 rounded shrink-0" />
-                            <Skeleton className="h-6 flex-1" />
+                    <CardContent className="flex items-center justify-between p-2">
+                        <div className="flex-1">
+                            <Skeleton className="h-12 w-full" />
                         </div>
-                        <div className="flex items-center">
-                            <Skeleton className="h-6 w-6 rounded" />
-                        </div>
+
                     </CardContent>
                 </Card>
             ))}
