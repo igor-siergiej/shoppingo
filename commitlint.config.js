785d9739df17aed642558993efc60a342c7538b4
Igor Siergiej <igorsiergiej@gmail.com>
2025-11-14 09:25:08 +0000

    added commit linting

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Customize rules if needed
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // New feature (triggers MINOR version bump)
+        'fix',      // Bug fix (triggers PATCH version bump)
+        'docs',     // Documentation only changes
+        'style',    // Code style changes (formatting, etc.)
+        'refactor', // Code refactoring
+        'perf',     // Performance improvements
+        'test',     // Adding or updating tests
+        'build',    // Build system changes
+        'ci',       // CI/CD configuration changes
+        'chore',    // Other changes that don't modify src
+        'revert',   // Revert a previous commit
+      ],
+    ],
+  },
+};
